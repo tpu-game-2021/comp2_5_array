dde2243c91b3bb304a59ac3b6689fc4e374c1572
yuto-0122 <ireina0122@icloud.com>
2021-10-17 16:42:48 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -12,7 +12,7 @@ void initialize(safe_array* ar, int n)
 	if (n <= 0) n = 1;// 非正の値が来たら、とりあえず1にする
 
 	ar->num = n;
-	ar->addr = (int*)malloc(sizeof(int) * n);
+	ar->addr = (int*)malloc(sizeof(int) * n);//おまじない、変数を初期化して変数を用意する。
 
 	if (ar->addr == NULL) ar->num = 0;// 確保できなかったら要素数は0とする
 }
@@ -28,6 +28,12 @@ void release(safe_array* ar)
 void resize(safe_array* ar, int n)
 {
 	// ToOo:配列の要素数を変更しよう！
+	ar->addr = (int*)realloc(ar->addr, sizeof(int) * n);
+	if (ar->addr == NULL)
+		ar->num = 0;
+	
+	ar->num = n;
+
 }
 
 // safe_array のindex番目の要素にvalを設定する
@@ -35,20 +41,39 @@ void resize(safe_array* ar, int n)
 bool set(const safe_array* ar, int index, int val)
 {
 	// ToOo:配列の要素を変更しよう！
-	return false;
+	
+	if (ar->num > index && 0 <= index)
+	{
+		ar->addr[index]= val;
+		
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 // safe_array のindex番目の要素を取得する
 // index が確保されていない場所を指していたら0を返す
 int get(const safe_array* ar, int index)
 {
-	// ToOo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	// ToOo:要素を取得して、indexがおかしかったら0を返そう
+	//if(ar->addr[index] == )
+	if (ar->num > index && 0 <= index)
+	{
+		return ar->addr[index];
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 // int_array の要素数を取得する
 int size(const safe_array* ar)
 {
 	// ToOo: 配列の要素数を返そう
-	return -1;
+	//numが要素数な為arの中のnumを返す。
+	return ar->num;
 }
