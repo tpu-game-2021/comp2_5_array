b383b16b2940996cc10f3595301a794f25430955
SugarWind <g2127037@st.t-kougei.ac.jp>
2021-10-17 21:44:31 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -28,6 +28,15 @@ void release(safe_array* ar)
 void resize(safe_array* ar, int n)
 {
 	// ToOo:配列の要素数を変更しよう！
+
+	ar->num = n;
+
+	ar->addr = (int*)realloc(ar->addr, sizeof(int) * n);
+	if (ar->addr == NULL)
+	{
+		ar->num = 0;
+	}
+
 }
 
 // safe_array のindex番目の要素にvalを設定する
@@ -35,7 +44,13 @@ void resize(safe_array* ar, int n)
 bool set(const safe_array* ar, int index, int val)
 {
 	// ToOo:配列の要素を変更しよう！
-	return false;
+	if (index >= ar->num || index < 0)
+	{
+		return false;
+	}
+
+	ar->addr[index] = val;
+	return true;
 }
 
 // safe_array のindex番目の要素を取得する
@@ -43,12 +58,19 @@ bool set(const safe_array* ar, int index, int val)
 int get(const safe_array* ar, int index)
 {
 	// ToOo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	if (index < 0 || index >= ar->num)
+	{
+		return 0;
+	}
+	else
+	{
+		return ar->addr[index];
+	}
 }
 
 // int_array の要素数を取得する
 int size(const safe_array* ar)
 {
 	// ToOo: 配列の要素数を返そう
-	return -1;
+	return ar->num;
 }
